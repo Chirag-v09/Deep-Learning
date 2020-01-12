ca76598d46d2f182aa996d58050415f89245f6cf
Chirag Verma <49785670+Chirag-v09@users.noreply.github.com>
2020-01-12 09:35:47 +0530

    Update MNIST Classification (Keras).py

--- MNIST Classification (Keras).py
+++ MNIST Classification (Keras).py
@@ -1,16 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 12 20:04:36 2019
-
-@author: Chirag
-"""
-
-
 '''   MNIST DATASETS   '''
 
+# Getting the data
 from keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
+# Reshaping (Data Preprocessing)
 train_images = train_images.reshape(60000,784)
 test_images = test_images.reshape(10000,784)
 
@@ -24,7 +18,9 @@ test_images = test_images.astype('float32')/255
 from keras import models
 from keras import layers
 
+# Getting the model
 model = models.Sequential()
+# Adding Some Layers
 model.add(layers.Dense(32, activation = 'relu', input_shape = (784, )))
 model.add(layers.Dense(10, activation = 'softmax'))
 
@@ -32,13 +28,8 @@ model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = [
 
 model.fit(train_images, train_labels, epochs = 10, batch_size = 64, validation_split = 0.1)
 
+# To save the Keras Model of name 'file.h5'
 model.save_weights('file.h5')
-model.load_weights('file.h5')
 
-'''   55% - 16777216 - 3.41s   '''
-'''   93% - 1024 - 4.98s   '''
-'''   94% - 512 - 6s   '''
-'''   95% - 256 - 6.87s   '''
-'''   95% - 255 - 7s   '''
-'''   95% - 128 - 9.65s   '''
-'''   96% - 64 - 16s   '''
+# Load the Keras Model of name 'file.h5'
+model.load_weights('file.h5')
