0208154f6d359c45444c04dcf290d0e366451ce7
Chirag Verma <cv091998@gmail.com>
2020-02-08 23:22:46 +0530

    Add files via upload

---
+++ iris TensorflowKeras.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 13:56:34 2020
+
+@author: Chirag
+"""
+
+import tensorflow as tf
+
+from sklearn.datasets import load_iris
+dataset = load_iris()
+
+X = dataset.data
+y = dataset.target
+
+from tensorflow import keras
+from tensorflow.keras import losses
+
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(128, activation = "tanh", input_shape = (4, )))
+model.add(keras.layers.Dense(32, activation = "relu"))
+model.add(keras.layers.Dense(3, activation = 'softmax'))
+
+model.compile("rmsprop", loss = losses.sparse_categorical_crossentropy, metrics = ["accuracy"])
+
+model.fit(X, y, epochs = 5)
+
+model.summary()
+
