8cd85ebd24d81540da146961f1d057d9175efd9a
Chirag Verma <cv091998@gmail.com>
2020-02-08 23:24:08 +0530

    Update ImageGenerator.py

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  5 19:35:52 2020
-
-@author: Chirag
-"""
-
 # Single Image
 
 import numpy as np
@@ -12,7 +5,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
 
-# Taking Mahindra car image here
 from tensorflow.keras.preprocessing.image import ImageDataGenerator\
 , array_to_img, img_to_array, load_img
 datagen = ImageDataGenerator(
@@ -22,12 +14,13 @@ datagen = ImageDataGenerator(
                                 height_shift_range = 0.2,
                                 shear_range = 0.2,
                                 zoom_range = 0.2,
-                                horizontal_flip = True)
+                                horizontal_flip = True )
 
 img = load_img("A/A1.jpg")
 x = img_to_array(img)
 x = x.reshape((1,) + x.shape)
 
+# Now in save dir write the dir name where the new generated images where saved
 i = 0
 for batch in datagen.flow(x, batch_size = 10, save_to_dir = 'ab generate', save_format = 'jpg'):
     i += 1
