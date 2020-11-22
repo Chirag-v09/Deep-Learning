5416ebd88d194072fbe05bc737ebcb760d117e75
Chirag Verma <cv091998@gmail.com>
2020-11-22 23:25:24 +0530

    Create object detection open-cv_v1.py

---
+++ object detection open-cv_v1.py
@@ -0,0 +1,15 @@
+from imageai.Detection import ObjectDetection
+
+detector = ObjectDetection()
+
+model_path = "./models/yolo-tiny.h5"
+input_path = "./input/test45.jpg"
+output_path = "./output/newimage.jpg"
+
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath(model_path)
+detector.loadModel()
+detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+
+for eachItem in detection:
+    print(eachItem["name"] , " : ", eachItem["percentage_probability"])
