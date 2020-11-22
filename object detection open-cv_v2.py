d3558712982005766cc33a9a1585ed40e6ddd6ed
Chirag Verma <cv091998@gmail.com>
2020-11-22 23:36:56 +0530

    Update object detection open-cv_v2.py

--- object detection open-cv_v2.py
+++ object detection open-cv_v2.py
@@ -8,3 +8,25 @@ output_image = draw_bbox(im, bbox, label, conf)
 plt.imshow(output_image)
 plt.show()
 print('Number of cars in the image is '+ str(label.count('car')))
+
+
+#############
+from platform import python_version
+import tensorflow
+import keras
+import cvlib as cv
+import cv2
+
+print('Python version: {}'.format(python_version()))
+print('cvlib version: {}'.format(cv.__version__))
+print('OpenCV version: {}'.format(cv2.__version__))
+print('Tensorflow version: {}'.format(tensorflow.__version__))
+print('Keras version: {}'.format(keras.__version__))
+
+
+# Uncomment this to read image from local folder/directory
+#im = cv2.imread('cars.jpeg')
+
+# Comment the above line and Uncomment the following two lines if you are reading imgae from a url
+#from skimage import io
+#im = io.imread('http://www.samacharnama.com/wp-content/uploads/2019/06/third-party-insurance..687.png')
